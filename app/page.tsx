9966a2f38a41c11b82f1b5c17e658314943d8a6c
jflores353 <jflores353@gmail.com>
2024-10-16 21:19:37 -0500

    Updates to layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Spades Scorekeeper</h1>
+        <h2>Player name will be here.</h2>
       </div>
     </main>
   );
